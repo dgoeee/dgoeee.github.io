f733a58afdf02dd684bdb2fabca897690123d892
dgoeee <68669003+dgoeee@users.noreply.github.com>
2023-05-01 14:36:08 +0800

    v1.0.5-beta7

---
+++ src/pages/HomepageFeatures/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'book1',
+    Svg: require('@site/static/img/书.svg').default,
+    description: (
+      <>
+        它确实是book的形式，不过内容更像一本好好整理过的笔记，可惜依旧比较杂乱，毕竟生活不是什么单一的事情
+      </>
+    ),
+  },
+  {
+    title: 'book2',
+    Svg: require('@site/static/img/书.svg').default,
+    description: (
+      <>
+        为了防止book1过于杂乱，这里准备了book2，它只是用来测试我的网页框架能够健康地容纳多本书
+      </>
+    ),
+  },
+  {
+    title: '赛博遗物',
+    Svg: require('@site/static/img/墓碑.svg').default,
+    description: (
+      <>
+        感谢墓碑记录一生，时间是我最大的敌人
+      </>
+    ),
+  },
+  {
+    title: '营火余音',
+    Svg: require('@site/static/img/柴火.svg').default,
+    description: (
+      <>
+        记下朋友们的声音，以及我的朋友们
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import HomepageFeatures from '@site/src/pages/HomepageFeatures';
+import HomepageHeader from '@site/src/pages/HomepageHeader';
+
+import styles from './index.module.css';
+
+
+function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+/* 主页的网页标签栏标题 */
+      title={`${siteConfig.title}'s Cyber Heritage`}
+      description="Here are DGoeee's Cyber Heritage. 井仪的赛博遗产（dgoeee.github.io)是静态资料网站，这里有经验为主的各类说明。">
+
+        <div  className={styles.hero_wrap} style={{
+          background: `url(./img/主页背景.svg) center center/cover no-repeat`
+        }}>
+      <HomepageHeader />
+        </div>
+
+      <main>
+
+        <div  className={styles.hero_wrap} style={{
+          background: `url(./img/主页背景.svg) center center/cover no-repeat`
+        }}>
+
+          <HomepageFeatures />
+
+        </div>
+
+      </main>
+
+
+
+    </Layout>
+  );
+}
+
+export default Home; 
