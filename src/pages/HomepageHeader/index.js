3f655c6a39411c34d3a106d867aa97154dae165b
dgoeee <68669003+dgoeee@users.noreply.github.com>
2023-05-03 19:32:03 +0800

    v1.0.5-beta10

--- src/pages/HomepageHeader/index.js
+++ src/pages/HomepageHeader/index.js
@@ -9,24 +9,44 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
-        <h5>
-          <font size="5">
-            流量费用警告：
-          </font>
-          绘画作品以及风景人物的照片等皆尽力达到壁纸级分辨率，单张大小可达20M<br />
-          它们不会影响页面的加载速度<br />
-        </h5>
+{/*
+*        <h4>
+*          <font size="5" style={{color: '#ffc8c8'}}>
+*            流量费用警告：
+*          </font>
+*          绘画作品以及风景人物的照片等皆尽力达到<span style={{color: '#ffc8c8'}}>壁纸级分辨率</span>，单张大小可达20M<br />
+*          它们不会影响页面的加载速度
+*        </h4>
+*/}
 
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/book1/封面">
-            前往井仪的笔记
+            to="/docs/book1/轮滑相关">
+            前往book1/轮滑相关
           </Link>
         </div>
+
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/book1/GPT问答">
+            前往book1/GPT问答
+          </Link>
+        </div>
+
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/book1/图床测试">
+            前往book1/图床测试
+          </Link>
+        </div>
+
       </div>
     </header>
   );
