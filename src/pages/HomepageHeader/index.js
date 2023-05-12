739673a5519da643bc4c0eddccab58106f9aee3e
dgoeee <dgoeee@163.com>
2023-05-12 20:42:56 +0800

    v1.0.5-beta10

--- src/pages/HomepageHeader/index.js
+++ src/pages/HomepageHeader/index.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./styles.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
 
-        <div className={styles.title}>
-          <h1 className="hero__title">{siteConfig.title}</h1>
-        </div>
-
-        <div className={styles.subtitle}>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-        </div>
-
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/book1/轮滑相关">
+            to="/docs/book1/轮滑相关"
+          >
             前往轮滑相关
           </Link>
         </div>
@@ -30,7 +25,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/book1/GPT问答">
+            to="/docs/book1/GPT问答"
+          >
             前往GPT问答
           </Link>
         </div>
@@ -38,7 +34,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/book1/图床测试">
+            to="/docs/book1/图床测试"
+          >
             前往图床测试
           </Link>
         </div>
@@ -46,11 +43,11 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/book1/本网页搭建过程">
+            to="/docs/book1/本网页搭建过程"
+          >
             本网页搭建过程
           </Link>
         </div>
-
       </div>
     </header>
   );
