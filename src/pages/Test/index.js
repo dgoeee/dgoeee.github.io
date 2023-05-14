e788dd09fb1b119750f68a33dd984ae34058eacf
dgoeee <dgoeee@163.com>
2023-05-14 12:41:18 +0800

    v1.0.5-beta10

---
+++ src/pages/Test/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+import styles from "./styles.module.css";
+
+function Test() {
+  return (
+    <div>
+      <div className="glass-container">
+        <image src="./img/logo.svg"></image>
+        测试
+      </div>
+    </div>
+  );
+}
+
+export default Test;
