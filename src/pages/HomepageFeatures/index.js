04d18de726bad00a4fee0ae72f1f3aa588023fe7
dgoeee <dgoeee@163.com>
2023-05-23 07:49:36 +0800

    v1.0.5-beta10

--- src/pages/HomepageFeatures/index.js
+++ src/pages/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 const FeatureList = [
   {
     title: "book1",
-    Svg: require("@site/static/img/书.svg").default,
+    Svg: require("@site/static/img/103.svg").default,
     description: (
       <>
         它确实是book的形式，不过内容更像一本好好整理过的笔记，可惜依旧比较杂乱，毕竟生活不是什么单一的事情
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: "book2",
-    Svg: require("@site/static/img/书.svg").default,
+    Svg: require("@site/static/img/421.svg").default,
     description: (
       <>
         为了防止book1过于杂乱，这里准备了book2，它只是用来测试我的网页框架能够健康地容纳多本书
@@ -23,12 +23,12 @@ const FeatureList = [
   },
   {
     title: "赛博遗物",
-    Svg: require("@site/static/img/墓碑.svg").default,
+    Svg: require("@site/static/img/420.svg").default,
     description: <>感谢墓碑记录一生，时间是我最大的敌人</>,
   },
   {
     title: "营火余音",
-    Svg: require("@site/static/img/柴火.svg").default,
+    Svg: require("@site/static/img/464.svg").default,
     description: <>记下朋友们的声音，以及我的朋友们</>,
   },
 ];
